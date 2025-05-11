d42227bc2f35591c349379a8bb27a39bffcf8cb9
mohanDevOps-arch <kandikuppa.krishna@herovired.com>
2025-05-11 12:18:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def get_by_name(name):
     return jsonify({"error": "No students found with the given name"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)

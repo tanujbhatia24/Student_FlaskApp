3981d45707898cb266587297dd412e3b0aedb156
Tanuj Bhatia <84853581+tanujbhatia24@users.noreply.github.com>
2025-05-22 20:12:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,6 @@ def get_by_name(name):
     return jsonify({"error": "No students found with the given name"}), 404
 
 if __name__ == '__main__':
+    #Enable CORS for all routes
+    CORS(app) 
     app.run(debug=True, host='0.0.0.0', port=5000)
